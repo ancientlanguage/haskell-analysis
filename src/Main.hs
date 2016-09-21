0d81f2bfb062e5880c5fa683b04b2d1bd12f059e
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2016-09-21 08:53:55 -0700

    Add tag function from xml conduit

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,18 @@
 module Main where
 
 import Sblgnt
+import XmlEvents
 import XmlParser (readParse)
 
 main :: IO ()
 main = do
---  let sblgntFile = "./data/xml-sblgnt/sblgnt.xml"
-  let sblgntFile = "./examples/sblgnt-test.xml"
+  let file = "./examples/sblgnt-test.xml"
+  events <- readEvents file
+  putStrLn . show $ events
+{-
+  let sblgntFile = "./data/xml-sblgnt/sblgnt.xml"
   parsedSblgnt <- readParse sblgntFile sblgnt
   case parsedSblgnt of
     Left x -> print x
     Right (xs, ys) -> putStrLn . show . length $ xs
+-}
