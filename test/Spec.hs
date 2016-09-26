5f3d9dd2ce90cbef641b29750d8b2943099f072b
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2016-09-26 12:55:23 -0700

    Add logging function to exe and test

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,13 +4,16 @@ import Test.HUnit hiding (Test)
 import qualified Sblgnt.Parser as Sblgnt
 import qualified Xml.Events as Xml
 import qualified Xml.Parser as Xml
+import qualified Xml.PositionTypes as Xml
+import Log
+
+logBook :: Xml.Element -> IO ()
+logBook = logElement (\x -> x == "book")
 
 parseSblgnt :: Test
 parseSblgnt = testCase "parse sblgnt" $ do
-  putStrLn "  beginning test"
   let xmlPath = "./data/xml-sblgnt/sblgnt.xml"
-  rootResult <- Xml.readRootElement xmlPath
-  putStrLn "  Xml.readRootElement"
+  rootResult <- Xml.readRootElement logBook xmlPath
   case rootResult of
     Left e -> assertFailure $ "Xml.readRootElement:\n" ++ show e
     Right root -> case Xml.parseRoot xmlPath Sblgnt.sblgnt root of
