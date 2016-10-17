0b85070bf49029787c298b01445cdef2fb06bc33
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2016-10-17 13:07:23 -0700

    Add test decode from binary data

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,23 @@
+import Test.Framework
+import Test.Framework.Providers.HUnit
+import Test.HUnit hiding (Test)
+import qualified Data.ByteString as BS
+import qualified Data.Serialize as Serialize
+import Primary
+import Lib
+
+decodeGroups :: BS.ByteString -> Either String [Group]
+decodeGroups = Serialize.decode
+
+testDecodeGroups :: Test
+testDecodeGroups = testCase "groups" $ do
+  let path = "./modules/binary-primary/data/groups.data"
+  encoded <- BS.readFile path
+  case decodeGroups encoded of
+    Left x -> assertFailure $ "decode failure:\n" ++ x
+    Right x -> return ()
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain
+  [ testGroup "Decode" [ testDecodeGroups ]
+  ]
