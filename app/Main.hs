97aba335de719b93db30358947f49765a0f14a7d
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2016-10-17 09:10:33 -0700

    Add stub code with deserialization

---
+++ app/Main.hs
@@ -0,0 +1,17 @@
+module Main where
+
+import qualified Data.ByteString as BS
+import qualified Data.Serialize as Serialize
+import Prepare.Source.Model
+import Lib
+
+decodeGroups :: BS.ByteString -> Either String [Group]
+decodeGroups = Serialize.decode
+
+main :: IO ()
+main = do
+  let path = "../data-breakfast/groups.data"
+  encoded <- BS.readFile path
+  case decodeGroups encoded of
+    Left x -> putStrLn x
+    Right x -> putStrLn . show . sum . fmap (length . groupSources) $ x
