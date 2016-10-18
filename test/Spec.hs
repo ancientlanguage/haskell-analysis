26388bda4114231abe0aa639a856e7189a6f14b5
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2016-10-18 12:04:33 -0700

    Add test for unicode-symbol map

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,8 @@
 import Test.Framework
 import Test.Framework.Providers.HUnit
 import Test.HUnit hiding (Test)
+import Grammar.Around
+import Grammar.Greek.Script.UnicodeSymbol
 import Grammar.Serialize
 
 testDecodeGroups :: Test
@@ -10,7 +12,23 @@ testDecodeGroups = testCase "groups" $ do
     Left x -> assertFailure $ "decode failure:\n" ++ x
     Right x -> return ()
 
+testAround
+  :: (Show a, Show e1, Show e2)
+  => Around e1 e2 a b
+  -> [a]
+  -> [Test]
+testAround (Around f g) = fmap testToFrom
+  where
+  testToFrom x = testCase ("to/from " ++ show x) $ do
+    case f x of
+      Left e1 -> assertFailure $ "to failure: " ++ show e1
+      Right y ->
+        case g y of
+          Left e2 -> assertFailure $ "from failure: " ++ show e2
+          Right _ -> return ()
+
 main :: IO ()
 main = defaultMain
   [ testGroup "Decode" [ testDecodeGroups ]
+  , testGroup "Unicode-Symbol" $ testAround unicodeSymbol "ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩαβγδεζηθικλμνξοπρσςτυφχψωα\x0300\x0301\x0308\x0313\x0314\x0342\x0345\x2019"
   ]
