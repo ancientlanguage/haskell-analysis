9746c9e8a243f0ca96cea58d124996b95adf5a63
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2016-10-19 06:34:42 -0700

    Add test of stage transitions

--- test/Greek.hs
+++ test/Greek.hs
@@ -3,12 +3,26 @@ module Greek where
 import Test.Framework
 import Test.Framework.Providers.HUnit
 import Test.HUnit hiding (Test)
+import Data.Either.Validation
 import Grammar.Greek.Script.UnicodeSymbol
+import Grammar.Greek.Stage
+import Grammar.Serialize
 import Around
+
+testStages :: Test
+testStages = testCase "around stages" $ do
+  result <- readGroups
+  case result of
+    Left x -> assertFailure $ "decode failure:\n" ++ x
+    Right gs ->
+      case stage0 (start gs) of
+        Failure x -> assertFailure $ "stage failure:\n" ++ show x
+        Success _ -> return ()
 
 greekGroups =
   [ testGroup "Unicode-Symbol" $ concat
     [ testAround "unicodeSymbol letters" unicodeSymbol "ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩαβγδεζηθικλμνξοπρσςτυφχψω"
     , testAround "unicodeSymbol marks" unicodeSymbol "α\x0300\x0301\x0308\x0313\x0314\x0342\x0345\x2019"
     ]
+  , testGroup "Stages" [ testStages ]
   ]
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,19 +1,7 @@
 import Test.Framework
 import Test.Framework.Providers.HUnit
 import Test.HUnit hiding (Test)
-import Grammar.Serialize
 import Greek
 
-testDecodeGroups :: Test
-testDecodeGroups = testCase "groups" $ do
-  result <- readGroups
-  case result of
-    Left x -> assertFailure $ "decode failure:\n" ++ x
-    Right x -> return ()
-
 main :: IO ()
-main = defaultMain $
-  [ testGroup "Decode" [ testDecodeGroups ]
-  ]
-  ++ 
-  greekGroups
+main = defaultMain greekGroups
