f4f8f299a03fa275b4039de61f083697686e8e68
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2016-11-08 10:04:39 -0800

    Use pattern synonyms to simplify some code

--- src/Grammar/Greek/Script/Rounds/Breathing.hs
+++ src/Grammar/Greek/Script/Rounds/Breathing.hs
@@ -18,11 +18,11 @@ breathing = makeRoundFwd to from
   to [] = Success ([], (PreserveMarks, (NoCrasis, NoInitialAspiration)))
   to (x : xs) = (\(x', (v, a)) xs' -> (x' : xs', (v, a))) <$> checkInitial x <*> allMedial xs
 
-  checkInitial (cs@[], (v, (a, Nothing))) = Success ((cs, (v, a)), (Unmarked, (NoCrasis, NoInitialAspiration)))
-  checkInitial (cs@[], (v, (a, Just B_Smooth))) = Success ((cs, (v, a)), (PreserveMarks, (NoCrasis, NoInitialAspiration)))
-  checkInitial (cs@[], (v, (a, Just B_Rough))) = Success ((cs, (v, a)), (PreserveMarks, (NoCrasis, HasInitialAspiration)))
-  checkInitial (cs@(_ : _), (v, (a, Nothing))) = Success ((cs, (v, a)), (PreserveMarks, (NoCrasis, NoInitialAspiration)))
-  checkInitial (cs@(_ : _), (v, (a, Just B_Smooth))) = Success ((cs, (v, a)), (PreserveMarks, (HasCrasis, NoInitialAspiration)))
+  checkInitial (cs@[] :^ v :^ a :^ Nothing) = Success $ (cs :^ v :^ a) :^ Unmarked :^ NoCrasis :^ NoInitialAspiration
+  checkInitial (cs@[] :^ v :^ a :^ Just B_Smooth) = Success $ (cs :^ v :^ a) :^ PreserveMarks :^ NoCrasis :^ NoInitialAspiration
+  checkInitial (cs@[] :^ v :^ a :^ Just B_Rough) = Success $ (cs :^ v :^ a) :^ PreserveMarks :^ NoCrasis :^ HasInitialAspiration
+  checkInitial (cs@(_ : _) :^ v :^ a :^ Nothing) = Success $ (cs :^ v :^ a) :^ PreserveMarks :^ NoCrasis :^ NoInitialAspiration
+  checkInitial (cs@(_ : _) :^ v :^ a :^ Just B_Smooth) = Success $ (cs :^ v :^ a) :^ PreserveMarks :^ HasCrasis :^ NoInitialAspiration
   checkInitial x = Failure [InvalidInitialBreathing x]
 
   allMedial [] = Success []
@@ -32,10 +32,10 @@ breathing = makeRoundFwd to from
   checkMedial x@(_, (_, (_, Just _))) = Failure [InvalidMedialBreathing x]
 
   from ([], _) = []
-  from ((cs, (v, a)) : xs, ci) = (cs, (v, (a, getBreathing cs ci))) : fmap (\(cs', (v', a')) -> (cs', (v', (a', Nothing)))) xs
+  from ((cs, (v, a)) : xs, ci) = cs :^ v :^ a :^ getBreathing cs ci : fmap (\(cs', (v', a')) -> cs' :^ v' :^ a' :^ Nothing) xs
 
   getBreathing :: [c] -> (MarkPreservation :* Crasis :* InitialAspiration) -> Maybe Breathing
-  getBreathing [] (PreserveMarks, (_, HasInitialAspiration)) = Just B_Rough
-  getBreathing [] (PreserveMarks, (_, NoInitialAspiration)) = Just B_Smooth
-  getBreathing (_ : _) (PreserveMarks, (HasCrasis, _)) = Just B_Smooth
+  getBreathing [] (PreserveMarks :^ _ :^ HasInitialAspiration) = Just B_Rough
+  getBreathing [] (PreserveMarks :^ _ :^ NoInitialAspiration) = Just B_Smooth
+  getBreathing (_ : _) (PreserveMarks :^ HasCrasis :^ _) = Just B_Smooth
   getBreathing _ _ = Nothing
