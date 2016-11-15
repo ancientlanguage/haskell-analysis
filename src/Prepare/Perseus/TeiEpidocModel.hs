c275b7d41076d18e528c73930e0aa04a1bab13df
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2016-11-15 09:33:15 -0800

    Finish tei epidoc model

--- src/Prepare/Perseus/TeiEpidocModel.hs
+++ src/Prepare/Perseus/TeiEpidocModel.hs
@@ -3,7 +3,65 @@ module Prepare.Perseus.TeiEpidocModel where
 import Data.Text (Text)
 import Prepare.Perseus.TeiEpidocHeaderModel
 
+data Milestone = Milestone
+  { milestoneUnit :: Text
+  , milestoneEd :: Text
+  }
+  deriving (Show)
+
+data Content
+  = ContentMilestone Milestone
+  | ContentText Text
+  | ContentAdd Text
+  | ContentCorr Text
+  | ContentDel Text
+  | ContentGap
+  deriving (Show)
+
+data Section = Section
+  { sectionNum :: Int
+  , sectionContent :: [Content]
+  }
+  deriving (Show)
+
+data Verse = Verse
+  { verseNum :: Int
+  , verseSections :: [Section]
+  }
+  deriving (Show)
+
+data Chapter = Chapter
+  { chapterNum :: Int
+  , chapterVerses :: [Verse]
+  }
+  deriving (Show)
+
+data Book = Book
+  { bookHead :: Text
+  , bookChapters :: [Chapter]
+  }
+  deriving (Show)
+
+data Edition = Edition
+  { editionN :: Text
+  , editionLang :: Text
+  }
+  deriving (Show)
+
+data Body = Body
+  { bodyEdition :: Edition
+  , bodyBooks :: [Book]
+  }
+  deriving (Show)
+
+data TeiText = TeiText
+  { teiTextLang :: Text
+  , teiTextBody :: Body
+  }
+  deriving (Show)
+
 data Tei = Tei
   { teiTeiHeader :: TeiHeader
+  , teiTeiText :: TeiText
   }
   deriving (Show)
