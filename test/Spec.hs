47338b171d04ebe5173641c1020226b6c90019f4
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2016-11-22 01:47:41 -0600

    Add tanach parsing tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -10,6 +10,7 @@ import qualified Prepare.Tanach.Paths as Paths
 import Prepare.Tanach.HeaderParser (header)
 import Prepare.Tanach.TanachParser (tanach)
 
+testParse :: TestName -> IO (Either [Char] t) -> Test
 testParse n f = testCase n $ do
   f >>= \case
     Left e -> assertFailure $ "loadParse failure:\n" ++ e
@@ -17,6 +18,15 @@ testParse n f = testCase n $ do
 
 parseTanachHeader :: Test
 parseTanachHeader = testParse "tanach header" $ loadParse Paths.headerFilePath header emptyLog
+
+parseAllTanach :: Test
+parseAllTanach = buildTestBracketed $ do
+  result <- loadParse Paths.indexFilePath index emptyLog
+  let
+    tests = case result of
+      Left x -> [testCase "parseAllTanach failure" $ assertFailure $ "index parse failure:\n" ++ x]
+      Right idx -> fmap (\x -> testParse x (loadParse x tanach emptyLog)) $ Paths.getAllFilePaths idx
+  return (testGroup "parseAllTanach" tests, return ())
 
 parseSblgnt :: Test
 parseSblgnt = testParse "parse sblgnt" $ loadParse xmlPath sblgnt logBook
@@ -30,5 +40,5 @@ main :: IO ()
 main = defaultMain
   [ testGroup "Perseus" (fmap parsePerseusTeiEpidoc perseusShortList)
   , testGroup "SBLGNT" [ parseSblgnt ]
-  , testGroup "Tanach Header" [ parseTanachHeader ]
+  , testGroup "Tanach Header" [ parseTanachHeader, parseAllTanach ]
   ]
